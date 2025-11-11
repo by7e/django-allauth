32c9c8fa332adcd598f173678fdac328eb4e556f
by7e <yamilo12345678910@gmail.com>
2025-11-11 02:42:47 +0100

    update/view profile urls added

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.home_view, name='home'),
+    path('accounts/profile/', views.profile_view, name='profile'),
+    path('accounts/profile/update/', views.update_profile, name='update_profile'),
 ]
