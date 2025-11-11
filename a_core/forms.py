db6ebdc02b71bbbb241e05be061b2a85b2f3fa04
by7e <yamilo12345678910@gmail.com>
2025-11-11 02:48:14 +0100

    user profile form

---
+++ a_core/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from a_core.models import UserProfile
+from django.contrib.auth import get_user_model 
+
+User = get_user_model()
+
+class UserProfileForm(forms.ModelForm):
+
+    bio = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 4, 'placeholder': 'Tell us a little bit about yourself...'}),
+        required=False,
+        label='Biography'
+    )
+    
+    # Customizing the profile_picture input for better user feedback
+    profile_picture = forms.ImageField(
+        label='Profile Picture (Optional)',
+        required=False,
+        help_text='Upload your profile picture.'
+    )
+
+    class Meta:
+        model = UserProfile
+        
+        fields = ['bio', 'profile_picture', 'age', 'location']
+        
+        widgets = {
+            'age': forms.NumberInput(attrs={'placeholder': 'Your age'}),
+            'location': forms.TextInput(attrs={'placeholder': 'City, Country or general location'}),
+        }
+        labels = {
+            'age': 'Age (Years)',
+            'location': 'Location',
+        }
