fa5c328d227f44d05f7618d3ddaf83ef5593569f
by7e <yamilo12345678910@gmail.com>
2025-11-11 02:41:27 +0100

    UserProfile model added

--- a_core/models.py
+++ a_core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
