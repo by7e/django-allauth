83e65deee17ee0dba4ee8665e534bb9946503f0b
by7e <yamilo12345678910@gmail.com>
2025-11-12 02:23:08 +0100

    cats listing / cat detail / cat add urls

--- a_core/urls.py
+++ a_core/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('accounts/profile/', views.profile_view, name='profile'),
     path('accounts/profile/update/', views.update_profile, name='update_profile'),
+    path('cats/add/', views.add_cat, name='add_cat'),
+    path('cats/', views.ListCats, name='list_cats'),
+    path('cat/<int:cat_id>/', views.cat_detail, name='cat_details'),
 ]
