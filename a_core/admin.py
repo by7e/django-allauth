45eef3030e8e6c04ab144269c5a6c606b333d7f5
by7e <yamilo12345678910@gmail.com>
2025-11-12 02:20:10 +0100

    register new models

--- a_core/admin.py
+++ a_core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from a_core.models import UserProfile, Cats, CatPicture, AdoptionRequest
 # Register your models here.
+
+admin.site.register(Permission)
+admin.site.register(ContentType)
+admin.site.register(UserProfile)
+admin.site.register(Cats)
+admin.site.register(CatPicture)
+admin.site.register(AdoptionRequest)
