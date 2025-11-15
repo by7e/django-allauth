d9d18ef613f0abfcaec8103acda09ab702938a34
by7e <yamilo12345678910@gmail.com>
2025-11-15 16:13:01 +0100

    others profile / adoption requests logic urls

--- a_core/urls.py
+++ a_core/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('accounts/profile/', views.profile_view, name='profile'),
     path('accounts/profile/update/', views.update_profile, name='update_profile'),
+    path('account/<str:user_id>/', views.other_profile_view, name='other_profile'),
     path('cats/add/', views.add_cat, name='add_cat'),
     path('cats/', views.ListCats, name='list_cats'),
     path('cat/<int:cat_id>/', views.cat_detail, name='cat_details'),
+    path('cat/<int:cat_id>/request_adoption/', views.request_adoption, name='request_adoption'),
+    path('adoption_request/<int:adoption_request_id>/cancel/', views.cancel_adoption_request, name='cancel_adoption_request'),
+    path('adoption_request/<int:adoption_request_id>/accept/', views.accept_adoption_request, name='accept_adoption_request'),
 ]
